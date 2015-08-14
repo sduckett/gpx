123730a6ad58b05af7b4e3280ce74fdafb4e4936
Sean Duckett <sduckett@gmail.com>
2015-08-14 15:58:57 -0600

    project blurbs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gpx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "What can you learn from a sequence of GPS Trackpoints?"
+  :url "https://github.com/sduckett/gpx"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.8.0"]
                  [org.clojure/algo.generic "0.1.2"]]
