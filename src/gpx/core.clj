95735393f0d8d6fc65f536073b95d49d3bb67d7a
Sean Duckett <sduckett@gmail.com>
2015-08-14 16:00:08 -0600

    what are the things?

--- src/gpx/core.clj
+++ src/gpx/core.clj
@@ -2,23 +2,24 @@
   (:require [clojure.xml :refer :all]
             [clojure.algo.generic.math-functions :refer :all]
             [clj-time.format :as tf]
-            [clj-time.core :as tc]
-            [clojure.pprint :refer [pprint]])
+            [clj-time.core :as tc])
   (:gen-class))
 
-(def R 6367)
- 
-(defn rad [x] 
+(def RADIUS-OF-EARTH-IN-METERS 6367)
+
+(defn rad [x]
   (* x (/ Math/PI 180)))
- 
+
 (defn meter [x] (* x 1000))
 
-;; https://gist.github.com/frankvilhelmsen/1787462
-(defn haversine [position destination]
-  (let [square_half_chord 
-          (+ (pow (sin (/ (rad (- (destination :lat) (position :lat))) 2)) 2) 
-             (* (cos (rad (position :lat))) 
-                (cos (rad (destination :lat))) 
+(defn haversine
+  "Compute the distance between two points on the Earth's surface.
+  See: <https://gist.github.com/frankvilhelmsen/1787462> and <https://en.wikipedia.org/wiki/Haversine_formula>"
+  [position destination]
+  (let [square_half_chord
+          (+ (pow (sin (/ (rad (- (destination :lat) (position :lat))) 2)) 2)
+             (* (cos (rad (position :lat)))
+                (cos (rad (destination :lat)))
                 (pow (sin (/ (rad (- (destination :lon) (position :lon))) 2)) 2)))
         angular_distance (* (asin (sqrt square_half_chord)) 2)]
     (* angular_distance RADIUS-OF-EARTH-IN-METERS)))
