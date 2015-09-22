3e1fd184085a9f1969afb140c0961f4bd23ffeb8
Sean Duckett <sduckett@gmail.com>
2015-09-22 14:18:21 -0600

    lein bikeshed:
    - cleanup long lines
    
    TODO:
    - add docstrings to bottom-level functions (currently at 25% docstrings)

--- src/gpx/geometry.clj
+++ src/gpx/geometry.clj
@@ -16,12 +16,17 @@
 
 (defn haversine
   "Compute the distance between two points on the Earth's surface.
-  See: <https://gist.github.com/frankvilhelmsen/1787462> and <https://en.wikipedia.org/wiki/Haversine_formula>"
+
+  See: <https://gist.github.com/frankvilhelmsen/1787462> and
+       <https://en.wikipedia.org/wiki/Haversine_formula>"
   [position destination]
   (let [square_half_chord
           (+ (pow (sin (/ (rad (- (destination :lat) (position :lat))) 2)) 2)
              (* (cos (rad (position :lat)))
                 (cos (rad (destination :lat)))
-                (pow (sin (/ (rad (- (destination :lon) (position :lon))) 2)) 2)))
+                (pow (sin (/ (rad (- (destination :lon)
+                                     (position :lon)))
+                             2))
+                     2)))
         angular_distance (* (asin (sqrt square_half_chord)) 2)]
     (* angular_distance RADIUS-OF-EARTH-IN-METERS)))
