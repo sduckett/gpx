fa3e27248673e1bcf9016611afad0b4d93382a9d
Sean Duckett <sduckett@gmail.com>
2015-09-29 13:06:21 -0600

    Check the haversine formula by factoring to greek

--- src/gpx/geometry.clj
+++ src/gpx/geometry.clj
@@ -16,18 +16,18 @@
   (/ (round (* n 100)) 100.0))
 
 (defn haversine
-  "Compute the distance between two points on the Earth's surface.
-
-  See: <https://gist.github.com/frankvilhelmsen/1787462> and
-       <https://en.wikipedia.org/wiki/Haversine_formula>"
-  [position destination]
-  (let [square_half_chord
-        (+ (pow (sin (/ (rad (- (destination :lat) (position :lat))) 2)) 2)
-           (* (cos (rad (position :lat)))
-              (cos (rad (destination :lat)))
-              (pow (sin (/ (rad (- (destination :lon)
-                                   (position :lon)))
-                           2))
-                   2)))
-        angular_distance (* (asin (sqrt square_half_chord)) 2)]
-    (* angular_distance RADIUS-OF-EARTH-IN-METERS)))
+  "The haversine distance between two geographical points 1 and 2;
+  see <https://en.wikipedia.org/wiki/Haversine_formula>"
+  [p1 p2]
+  (let [φ1 (p1 :lat)
+        λ1 (p1 :lon)
+        φ2 (p2 :lat)
+        λ2 (p2 :lon)
+        Δλ (Math/toRadians (- λ2 λ1))
+        Δφ (Math/toRadians (- φ2 φ1))
+        ϴ (+ (Math/pow (Math/sin (/ Δφ 2)) 2)
+             (* (Math/cos (Math/toRadians φ1))
+                (Math/cos (Math/toRadians φ2))
+                (Math/pow (Math/sin (/ Δλ 2)) 2)))
+        Δσ (* 2 (Math/asin (Math/sqrt ϴ)))]
+    (* Δσ RADIUS-OF-EARTH-IN-METERS)))
--- test/gpx/geometry_test.clj
+++ test/gpx/geometry_test.clj
@@ -6,6 +6,12 @@
   (testing "rounding of numbers is sane"
     (is (= 42.02 (round2 42.01983)))))
 
+(deftest test-haversine-distance
+  (testing "the distance between DEN and AUS"
+    (let [den {:lat 39.861111 :lon -104.673056}
+          aus {:lat 30.194444 :lon  -97.670000}]
+      (is (= 1247.99 (round2 (haversine den aus)))))))
+
 #_(deftest test-cider-and-precise-numbers
     (testing "if this still crashes cider via the nrepl-server ;)"
       (is (= 42.02 (round2 42.0198723645983645823)))))
